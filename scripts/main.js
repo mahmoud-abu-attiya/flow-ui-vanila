c2c535dc731de51f0bd33bfdaddc0a155183485b
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2022-12-10 01:51:11 +0200

    handle backend validation

--- scripts/main.js
+++ scripts/main.js
@@ -15,8 +15,11 @@ const password = document.getElementById("password");
 const password_confirmation = document.getElementById("password_confirmation");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
-const formBtnText = document.querySelector("#signupForm button[type='submit'] .text");
+const formBtnText = document.querySelector(
+   "#signupForm button[type='submit'] .text"
+);
 const eye = document.querySelector(".eye");
+const errors = document.querySelectorAll(".error");
 
 const seePasswordToggle = () => {
    let password = document.getElementById("password");
@@ -35,13 +38,15 @@ const seePasswordToggle = () => {
       seePassword = true;
    }
 };
-const usernameError = () => {
+const usernameError = (massage) => {
    usernameErr.classList.remove("hidden");
    username.classList.add("border-red-500");
+   usernameErr.innerHTML += `<span class="block">${massage}</span>`;
 };
-const emailError = () => {
+const emailError = (massage) => {
    emailErr.classList.remove("hidden");
    email.classList.add("border-red-500");
+   emailErr.innerHTML += `<span class="block">${massage}</span>`;
 };
 const passwordError = (messages) => {
    passwordErr.classList.remove("hidden");
@@ -49,10 +54,11 @@ const passwordError = (messages) => {
    password_confirmation.classList.add("border-red-500");
    passwordErr.innerHTML += `<span class="block">${messages}</span>`;
 };
-const matchError = () => {
+const matchError = (messages) => {
    matchErr.classList.remove("hidden");
    password_confirmation.classList.add("border-red-500");
    password.classList.add("border-red-500");
+   matchErr.innerHTML += `<span class="block">${messages}</span>`;
 };
 const setLoading = () => {
    loadingEl.classList.remove("hidden");
@@ -70,6 +76,20 @@ const passwordErrorMassages = (errors) => {
    password_confirmation.classList.add("border-red-500");
    stopLoading();
 };
+const emailErrorMassages = (errors) => {
+   errors.forEach((error) => {
+      emailError(error);
+   });
+   email.classList.add("border-red-500");
+   stopLoading();
+};
+const usernameErrorMassages = (errors) => {
+   errors.forEach((error) => {
+      usernameError(error);
+   });
+   username.classList.add("border-red-500");
+   stopLoading();
+};
 
 form.onsubmit = (e) => {
    let formData = new FormData(form);
@@ -83,6 +103,14 @@ form.onsubmit = (e) => {
    } else {
       stopLoading();
    }
+
+   // clear error messages
+   errors.forEach((error) => {
+      error.classList.add("hidden");
+   });
+   form.querySelectorAll("input").forEach((input) => {
+      input.classList.remove("border-red-500");
+   });
 
    // validate username
    let usernameRegex = /^\D[a-z|0-9]{3,15}\D$/gi;
@@ -103,15 +131,17 @@ form.onsubmit = (e) => {
    // set error messages
    switch (false) {
       case usernameValidations:
-         usernameError();
+         usernameError([
+            "Username must be between 5 and 15 characters and can only contain letters and numbers and can not start or end with numbers.",
+         ]);
          stopLoading();
          break;
       case emailValidations:
-         emailError();
+         emailError("Please enter a valid email address.");
          stopLoading();
          break;
       case matchPassword:
-         matchError();
+         matchError("Password and password confirmation must match.");
          stopLoading();
          break;
       default:
@@ -127,7 +157,7 @@ form.onsubmit = (e) => {
          method: "POST",
          headers: {
             "Content-Type": "application/json",
-            accept: "application/json",
+            Accept: "application/json",
          },
          body: JSON.stringify(data),
       })
@@ -138,9 +168,13 @@ form.onsubmit = (e) => {
             // password error from backend
             if (data.errors?.password) {
                passwordErrorMassages(data.errors.password);
+            } else if (data.errors?.email) {
+               emailErrorMassages(data.errors.email);
+            } else if (data.errors?.username) {
+               usernameErrorMassages(data.errors.username);
             } else {
                localStorage.setItem("email", data.email);
-               window.location.href = "/flow-ui-vanila/loggedin.html";
+               window.location.href = "./loggedin.html";
             }
          })
          .catch((error) => {
@@ -152,25 +186,12 @@ form.onsubmit = (e) => {
    }
 };
 
-username.oninput = () => {
-   usernameErr.classList.add("hidden");
-   username.classList.remove("border-red-500");
-};
-
-email.oninput = () => {
-   emailErr.classList.add("hidden");
-   email.classList.remove("border-red-500");
-};
-
-password.oninput = () => {
-   passwordErr.classList.add("hidden");
-   password.classList.remove("border-red-500");
-};
-
-password_confirmation.oninput = () => {
-   matchErr.classList.add("hidden");
-   password_confirmation.classList.remove("border-red-500");
-};
+form.querySelectorAll("input").forEach((input) => {
+   input.oninput = () => {
+      input.classList.remove("border-red-500");
+      input.closest(".form-control").querySelector(".error").classList.add("hidden");
+   };
+});
 
 seePasswordEl.onclick = () => {
    seePasswordToggle();
